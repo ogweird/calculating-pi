901b9feb10aa36753d254d6340b728cf0ab11676
weird <jan7sitarski@outlook.com>
2024-08-28 18:46:47 +0200

    Added main.hpp

---
+++ main.hpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+
+class c_pi {
+public:
+	double leibniz(double n) {
+		double pi = 0;
+		double sign = 1;
+
+		for (double i = 1; i <= (n * 2); i += 2) {
+			pi = pi + sign * (4 / i);
+			sign = -sign;
+		}
+
+		return pi;
+	}
+
+	double wallis(double n) {
+		double pi = 4.;
+
+		for (double i = 3; i <= (n + 2); i += 2) {
+			pi = pi * ((i - 1) / i) * ((i + 1) / i);
+		}
+
+		return pi;
+	}
+
+	double nilakantha(double n) {
+		double pi = 3;
+		double sign = 1;
+
+		for (double i = 2; i <= (n * 2); i += 2) {
+			pi = pi + sign * (4 / (i * (i + 1) * (i + 2)));
+			sign = -sign;
+		}
+
+		return pi;
+	}
+};
+
+inline auto pi = new c_pi;
